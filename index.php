f5f37070ce9ef01d2212da838a467a3e28347bfa
mcboni <47659845+mcboni@users.noreply.github.com>
2021-11-25 15:56:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/landing.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<title>Oggongo Geospatial</title>
+</head>
+
+
+<body>
+
+	<header>
+
+		<nav class="navbar navbar-expand-md">
+
+			<!-- Inserting nav brand Image beofre brand title-->
+			<img id="log" src="css/logo1.png"> <a id="fff2" class="navbar-brand" href="#">Oggongo GPIL</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="nav navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a id="fff" class="nav-link" href="contact.php">Contact Us<span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a id="fff" class="nav-link" href="products.php">Products</a>
+					</li>
+					<li class="nav-item">
+						<a id="fff" class="nav-link" href="services-Eko-Smart-City.php">Services</a>
+					</li>
+					<li class="nav-item">
+						<a id="fff" class="nav-link" href="products.php">Find a Place</a>
+					</li>
+					<li class="nav-item">
+						<a id="fff" class="nav-link" href="blog.php">Blog</a>
+					</li>
+				</ul>
+				<!--Inserting Signin Link-->
+				<a href="Signup.php"><img src="css/signin.png" id="sign1"></a>
+			</div>
+		</nav>
+	</header>
+
+	<main>
+		<div class="container-fluid">
+			<section>
+				<div class="row top-section">
+					<div class="col-lg-12 col-md-12 col-sm-12 text-center">
+						<h1 id="explore">Eko Smart City</h1>
+					</div>
+
+					<div class="col-lg-12 col-md-12 col-sm-12 text-center">
+						<h4 id="explore2" data-aos="zoom-in" data-aos-duration="2000"> smart utility management | smart revenue generation | smart delivery services</h4>
+					</div>
+
+
+				</div>
+			</section>
+
+
+			<section>
+				<div class="row abt-section">
+					<div class="col-lg-6 col-md-6 col-sm-12">
+						<img src="css/abt_img.png" class="img-fluid" id="abt_img" />
+					</div>
+
+					<div class="col-lg-6 col-md-6 col-sm-12 abt_text" data-aos="fade-left" data-aos-duration="3000">
+						<h2 id="abt_text">About Us</h2>
+						<p id="abt_des"> Oggongo Geospatial Platform and Intelligence Limited (OGPIL) is a specialist geospatial support services provider that works with Asset Intensive
+							Organizations and Products & Services providers to help them increase revenue generation from their assets. We have capacity to provide
+							geospatial knowledge platforms, interfaced with our clients’ legacy datasets and provide standard datasets for our clients operations. OGPIL works
+							directly with asset owners and partners with other service providers to enhance their products and services.
+							Oggongo GPIL has the ambitious goal of providing geo-coding, geo-information and resource solutions services across the West African sub region, starting with Lagos, Nigeria.
+						</p>
+					</div>
+
+				</div>
+			</section>
+
+
+			<section>
+				<div class="row service_section">
+					<div class="col-lg-12 col-md-12 col-sm-12 text-center">
+						<h1 id="heading_planA"> Services At a Glance</h1>
+					</div>
+
+					<div class="col-md-4 col-lg-4 col-sm-12" data-aos="flip-left" data-aos-duration="1000">
+
+						<div class="card">
+							<img class="card-img-top" src="css/i4.jpg" id="card-image" alt="Card image cap">
+							<div class="card-head">
+								<strong style="color:black;">Product and Service Delivery Services</strong>
+							</div>
+							<div class="card-body">
+								<p class="card-text2">Using our building dataset containing well enumurated properties and roads, delivery and logistic is made easier</p>
+								<a href="#" class="btn" id="butt1">Read More...</a>
+							</div>
+						</div>
+					</div>
+
+
+
+					<div class="col-md-4 col-lg-4 col-sm-12  " data-aos="zoom-in" data-aos-duration="1000">
+
+						<div class="carde1">
+							<img class="card-img-top" src="css/i5.jpg" id="card-image" alt="Card image cap">
+							<div class="card-head">
+								<strong>Utility Management</strong>
+							</div>
+							<div class="card-body">
+								<p class="card-text">Our utility and infrastructure services provides up to date big data support for transportation and elecricity assets</p>
+								<a href="#" class="btn" id="butt">Read More...</a>
+							</div>
+						</div>
+					</div>
+
+
+
+					<div class="col-md-4 col-lg-4 col-sm-12" data-aos="flip-right" data-aos-duration="1000">
+						<div class="carde2">
+							<img class="card-img-top" src="css/i6.jpg" id="card-image" alt="Card image cap">
+							<div class="card-head">
+								<strong style="color:black;">Smart City Support Services</strong>
+							</div>
+							<div class="card-body">
+								<p class="card-text1">Our smart city support services provide I.T and earth resource platform to improve, facilitate and increase operational efficiency and quality within Lagos state</p>
+								<a href="#" class="btn" id="butt2">Read More...</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+			<section>
+				<div class="row photo-section">
+					<div class="col-lg-12 col-md-12 col-sm-12 text-center gallery_heading">
+						<img src="css/photo.png" id="pg_logo">
+						<h1 id="p_g">Media Feed</h1>
+					</div>
+
+					<div class="col-lg-6 col-md-6 col-sm-12 text-center gallery_body">
+
+						<iframe id="frame" src="https://www.youtube.com/embed/QCwI27-Q9tE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+					</div>
+
+
+					<div class="col-lg-6 col-md-6 col-sm-12 text-center gallery_body">
+						<iframe id="frame" src="https://www.youtube.com/embed/xaLnsBOsG0A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					</div>
+
+
+
+
+
+				</div>
+
+
+			</section>
+
+
+
+
+			<section>
+				<div class="row cardintro_section">
+					<div class="col-lg-12 col-md-12 col-sm-12 text-center">
+						<h1 id="heading_plan"> How Well Do You Know Lagos?</h1>
+						<p id="heading_plan2">Visit our blog for mouth watering historical reads about Lagos</p>
+					</div>
+					<div class="col-md-6 col-lg-6 col-sm-12" data-aos="fade-right" data-aos-duration="1000">
+						<img class="main_pic2" src="css/main4.png" alt="Card image cap">
+					</div>
+
+
+					<div class="col-md-6 col-lg-6 col-sm-12 text-center">
+						<P id="intro_p" data-aos="zoom-in" data-aos-duration="1000">Lagos is rich in culture and lifestyle. The blog brings to you seasoned write ups about Lagos you
+							wouldnt believe exsisted!!! </P>
+						<a href="blog.php"><input type="submit" name="txt" value="Visit the Blog" id='submit' data-aos="zoom-in" data-aos-duration="3000"></a>
+					</div>
+
+				</div>
+			</section>
+
+
+
+
+
+
+			<section>
+				<div class="row newsletter">
+					<div class="col-lg-12 col-md-12 col-sm-12 text-center input">
+						<h6 id="newsletter">Subscribe to our newsletter to get updated</h6>
+						<input type="text" class="form-control" id="input_box" placeholder="Email address" />
+						<button type="button" class="btn4">Subscribe</button>
+					</div>
+				</div>
+			</section>
+
+			<footer>
+				<div class="row footer">
+					<div class="col-sm-12 col-md-4 col-lg-4">
+						<div class="row">
+
+							<div class="col-sm-12 col-md-12 col-lg-12 comp_logo">
+								<img src="css/ogp.png" id="img_logo">
+								<h5 id="comp_logo">Oggongo GPIL</h5>
+							</div>
+
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-4 col-lg-4 soc">
+						<h3 id="soc_name">Follow us on social media</h3>
+						<img src="css/ig.png" id="icon-ig">
+						<img src="css/fb.png" id="icon-fb">
+						<img src="css/li.png" id="icon-li">
+						<p id="handle">@oggongoGPIL
+					</div>
+
+					<div class="col-sm-12 col-md-4 col-lg-4 links">
+						<div class="row">
+							<div class="col-lg-4 col-md-4 col-sm-12">
+								<p id="contact"><a id="cs" href="contact.php">Contact us</a></p>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-12">
+								<p id="service"><a id="cs" href="services-Eko-Smart-City.php">Services</a></p>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-12">
+								<p id="product"><a id="cs" href="products">Products</a></p>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-12">
+								<p id="pricing"><a id="cs" href="#">Pricing</a></p>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-12">
+								<p id="blog"><a id="cs" href="blog.php">Blog</a></p>
+							</div>
+
+						</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12">
+						<h6 id="rights">All right reserved to oggongoGPIL</h6>
+
+					</div>
+
+				</div>
+
+
+			</footer>
+
+
+
+
+
+
+
+
+		</div>
+	</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<script>
+		! function(e, t, a) {
+			var c = e.head || e.getElementsByTagName("head")[0],
+				n = e.createElement("script");
+			n.async = !0, n.defer = !0, n.type = "text/javascript", n.src = t + "/static/js/chat_widget.js?config=" + JSON.stringify(a), c.appendChild(n)
+		}(document, "https://app.engati.com", {
+			bot_key: "6ac0daf12ba54c42",
+			welcome_msg: true,
+			branding_key: "default",
+			server: "https://app.engati.com",
+			e: "p"
+		});
+	</script>
+</body>
+<!-- jQuery first-->
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+<!--- Script for bootstrap-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script type="text/javascript" src="aos.js"></script>
